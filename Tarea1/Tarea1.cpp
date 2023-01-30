6cc81c3e06c8f51f5ab871cd1b39f68a94e79fb6
A01794299 <114030758+A01794299@users.noreply.github.com>
2023-01-29 23:09:18 -0600

    Add files via upload

---
+++ Tarea1/Tarea1.cpp
@@ -0,0 +1,50 @@
+// Tarea1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+
+#include <iostream>
+#include <omp.h>
+#include <stdio.h>
+#include  <stdlib.h>
+
+#define N 1000
+#define chunk 20
+#define mostrar 10
+
+void imprimeArreglo(float *d);
+
+int main()
+{
+	std::cout << "Sumando arreglos en paralelo:\n";
+	float a[N], b[N], c[N];
+	int i;
+
+	for (i = 0; i < N; i++)
+	{
+		a[i] = i * 2;
+		printf(a[i]);
+		b[i] = (i +4) * 2;
+
+	}
+	int pedazos = chunk;
+	printf(a);
+
+#pragma omp parallel for shared(a,b,c,pedazos) private(i) schedule(static,pedazos)
+
+	for (i = 0; i < N; i++)
+		c[i] = a[i] + b[i];
+
+	std::cout << "imprimiendo los primeros" << mostrar << " valores del arreglo a: " << std::endl;
+	imprimeArreglo(a);
+	std::cout << "imprimiendo los primeros" << mostrar << " valores del arreglo b: " << std::endl;
+	imprimeArreglo(b);
+	std::cout << "imprimiendo los primeros" << mostrar << " valores del arreglo c: " << std::endl;
+	imprimeArreglo(c);
+}
+
+void imprimeArreglo(float *d)
+{
+	for (int x = 0; x < mostrar; x++)
+		std::cout << d[x] << "-";
+	std::cout << std::endl;
+}
